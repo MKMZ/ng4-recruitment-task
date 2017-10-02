a1f1f0f809db0e5bee0a3ce6fc553ffad0410917
MKMZ <rafal.klimek@student.uj.edu.pl>
2017-10-02 15:00:05 +0200

    REFACTOR: removal of unnecessary models

--- src/shared/fake.repository.ts
+++ src/shared/fake.repository.ts
@@ -4,10 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Post } from 'content/posts/post';
-import { Comment } from 'content/comments/comment';
 import { User } from 'content/users/user';
-import { Album } from 'content/albums/album';
-import { Photo } from 'content/photos/photo';
 
 
 @Injectable()
